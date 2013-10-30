29709c7703c53bdc79ca5eeea9ec2b53632262ab
William Lindsay <william.fr.lindsay@gmail.com>
2013-10-30 10:54:26 -0400

    Fix for Timeout Error for Ubigraph

--- RubiGraphBuilder.rb
+++ RubiGraphBuilder.rb
@@ -6,11 +6,11 @@ class RubiGraphBuilder
   #
   # Member Variables and static variables
   #
-  @@nodes = {}
-  @@edge_objects = {}
-  @@edge_counter = {}
-  @@communities = {}
-  @@community_color = {}
+  NODES = {}
+  EDGE_OBJECTS = {}
+  EDGE_COUNTER = {}
+  COMMUNITIES = {}
+  COMMUNITY_COLOR = {}
   @@counter = 0
   COLORS = ["#FF00FF", "#00FF00", "#CC00FF", "#33FF00", "#9900FF", "#66FF00", "#6600FF", "#99FF00", "#3300FF", "#CCFF00", "#0000FF", "#FFFF00", "#FF00CC", "#00FF33", "#FF33FF", "#33FF33", "#CC33FF", "#66FF33", "#9933FF", "#99FF33", "#6633FF", "#CCFF33", "#3333FF", "#FFFF33", "#0033FF", "#FFCC00", "#FF0099", "#00FF66", "#FF33CC", "#33FF66", "#FF66FF", "#66FF66", "#CC66FF", "#99FF66", "#9966FF", "#CCFF66", "#6666FF", "#FFFF66", "#3366FF", "#FFCC33", "#0066FF", "#FF9900", "#FF0066", "#00FF99", "#FF3399", "#33FF99", "#FF66CC", "#66FF99", "#FF99FF", "#99FF99", "#CC99FF", "#CCFF99", "#9999FF", "#FFFF99", "#6699FF", "#FFCC66", "#3399FF", "#FF9933", "#0099FF", "#FF6600", "#FF0000", "#00FFCC", "#FF3333", "#33FFCC", "#FF6666", "#66FFCC", "#FF9999", "#99FFCC", "#FFCCCC", "#CCFFCC", "#CCFFFF", "#FFFFCC", "#99FFFF", "#FFCC99", "#66FFFF", "#FF9966", "#33FFFF", "#FF6633", "#00FFFF", "#FF3300", "#FFFFFF"]
 
@@ -26,7 +26,7 @@ class RubiGraphBuilder
     end
     file.close
     
-    # Read original graph file and add all @@nodes and edges
+    # Read original graph file and add all NODES and edges
     file = File.open("output/graph.dot", "r")
     while (line = file.gets)
       info = "#{line}".split(' ')
@@ -43,11 +43,11 @@ class RubiGraphBuilder
   #
   def addToCommunity(node, community)
     communities = {node => community}
-    @@communities.merge!(communities)
+    COMMUNITIES.merge!(communities)
     
-    if @@community_color[community].nil?
+    if COMMUNITY_COLOR[community].nil?
       newColor = {community => COLORS[@@counter]}
-      @@community_color.merge!(newColor)
+      COMMUNITY_COLOR.merge!(newColor)
       
       @@counter += 1
       if @@counter >= COLORS.length
@@ -60,16 +60,16 @@ class RubiGraphBuilder
   # Adds the given node to the graph
   #
   def addNode(node)
-    if @@nodes[node].nil?
+    if NODES[node].nil?
       newNode = {node => Rubigraph::Vertex.new}
-      @@nodes.merge!(newNode)
-      @@nodes[node].shape = 'sphere'
-      if !@@communities[node].nil?
-        @@nodes[node].color = @@community_color[@@communities[node]]
+      NODES.merge!(newNode)
+      NODES[node].shape = 'sphere'
+      if !COMMUNITIES[node].nil?
+        NODES[node].color = COMMUNITY_COLOR[COMMUNITIES[node]]
       end
       
       newCounter = { node => 0}
-      @@edge_counter.merge!(newCounter)
+      EDGE_COUNTER.merge!(newCounter)
     end
   end
   
@@ -77,16 +77,16 @@ class RubiGraphBuilder
   # Adds and edge between the two given nodes
   #
   def addEdge(node0, node1)
-    if !node0.nil? && !node1.nil? && @@edge_objects[node1 + " -> " + node0].nil? && @@edge_objects[node0 + " -> " + node1].nil?
-      newEdgeObject = {node0 + " -> " + node1 => Rubigraph::Edge.new(@@nodes[node0], @@nodes[node1])}
-      @@edge_objects.merge!(newEdgeObject)
+    if !node0.nil? && !node1.nil? && EDGE_OBJECTS[node1 + " -> " + node0].nil? && EDGE_OBJECTS[node0 + " -> " + node1].nil?
+      newEdgeObject = {node0 + " -> " + node1 => Rubigraph::Edge.new(NODES[node0], NODES[node1])}
+      EDGE_OBJECTS.merge!(newEdgeObject)
       
-      if @@communities[node0] == @@communities[node1]
-        @@edge_objects[node0 + " -> " + node1].color = @@community_color[@@communities[node0]]
+      if COMMUNITIES[node0] == COMMUNITIES[node1]
+        EDGE_OBJECTS[node0 + " -> " + node1].color = COMMUNITY_COLOR[COMMUNITIES[node0]]
       end
       
-      newCounter = { node0 => (@@edge_counter[node0] + 1), node1 => (@@edge_counter[node1] + 1)}
-      @@edge_counter.merge!(newCounter)
+      newCounter = { node0 => (EDGE_COUNTER[node0] + 1), node1 => (EDGE_COUNTER[node1] + 1)}
+      EDGE_COUNTER.merge!(newCounter)
     end
   end
   
@@ -94,9 +94,9 @@ class RubiGraphBuilder
   # Removes the given node if there aren't any other edges going to it
   #
   def removeNode(node)
-    if @@edge_counter[node] <= 0 && !@@nodes[node].nil?
-      @@nodes[node].remove
-      @@nodes.delete(node)
+    if EDGE_COUNTER[node] <= 0 && !NODES[node].nil?
+      NODES[node].remove
+      NODES.delete(node)
     end
   end
   
@@ -104,12 +104,12 @@ class RubiGraphBuilder
   # Removes the given edge and checks both variations of it
   #
   def removeEdge(node0, node1)
-    if !@@edge_objects[node0 + " -> " + node1].nil?
-      @@edge_objects[node0 + " -> " + node1].remove
-      @@edge_objects.delete(node0 + " -> " + node1)
-      
-      newCounter = {node0 => (@@edge_counter[node0] - 1), node1 => (@@edge_counter[node1] - 1)}
-      @@edge_counter.merge!(newCounter)
+    if !EDGE_OBJECTS[node0 + " -> " + node1].nil?
+      EDGE_OBJECTS[node0 + " -> " + node1].remove
+      EDGE_OBJECTS.delete(node0 + " -> " + node1)
+  
+      newCounter = {node0 => (EDGE_COUNTER[node0] - 1), node1 => (EDGE_COUNTER[node1] - 1)}
+      EDGE_COUNTER.merge!(newCounter)
     end
   end
 end
